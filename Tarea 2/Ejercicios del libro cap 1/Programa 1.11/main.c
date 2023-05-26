b1961b3752a71e6fcf893b59d9895d5f22c41459
Freddy Guerrero <freddysguerrero.fg@gmail.com>
2023-05-25 23:24:55 -0400

    Tarea 2 programacion para mecatronicos

--- Tarea 2/Ejercicios del libro cap 1/Programa 1.11/main.c
+++ Tarea 2/Ejercicios del libro cap 1/Programa 1.11/main.c
@@ -14,10 +14,10 @@ void main (void)
     scanf(" %f %f", &X2,&Y2);
     printf("Ingrese la coordenada del punto 3: ");
     scanf(" %f %f", &X3,&Y3);
-    LA1 = (pow(X1-X2, 2) + (Y1-Y2, 2));
-    LA2 = (pow(X2-X3, 2) + (Y2-Y3, 2));
-    LA3 = (pow(X1-X3, 2) + (Y1-Y3, 2));
-    PER = (LA1 + LA2 + LA3);
+    LA1 = sqrt(pow(X1-X2, 2) + (Y1-Y2, 2));
+    LA2 = sqrt(pow(X2-X3, 2) + (Y2-Y3, 2));
+    LA3 = sqrt(pow(X1-X3, 2) + (Y1-Y3, 2));
+    PER = sqrt(LA1 + LA2 + LA3);
     printf("\nEl perimetro del triangulo es: %6.3f", PER);
 }
 
